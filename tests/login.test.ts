23b2e928895855dcb2265aa3b2ce1bf5848e2ed9
ola-gran <57948855+ola-gran@users.noreply.github.com>
2025-11-24 18:17:22 +0100

    comment out tests

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -9,40 +9,41 @@ import { LoginPage } from "../pages/loginPage";
   
   let password: string;
 
-  test('Verify Role', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
+//   test('Verify Role', async ({ page }) => {
+//     const loginPage = new LoginPage(page);
+//     await loginPage.goto();
 
-    // 1️⃣ Kontrollera att dropdown innehåller rätt alternativ
-    //const expectedOptions = ['Business', 'Consumer']; // ändra enligt vad sidan faktiskt har text i inner-HTML
-    //await loginPage.assertRoleOptions(expectedOptions);
+//     // 1️⃣ Kontrollera att dropdown innehåller rätt alternativ
+//     //const expectedOptions = ['Business', 'Consumer']; // ändra enligt vad sidan faktiskt har text i inner-HTML
+//     //await loginPage.assertRoleOptions(expectedOptions);
 
-    // 2️⃣ Kontrollera vilket värde som är valt som standard
-    //set value first:
-    await loginPage.setRole('Consumer');            // värdet som visas/"fyll i"/inner-html
-    await loginPage.assertSelectedRole('consumer'); // ändra enligt default-värdet på sidan value-namnet (typ id)
-  });
+//     // 2️⃣ Kontrollera vilket värde som är valt som standard
+//     //set value first:
+//     await loginPage.setRole('Consumer');            // värdet som visas/"fyll i"/inner-html
+//     await loginPage.assertSelectedRole('consumer'); // ändra enligt default-värdet på sidan value-namnet (typ id)
+//   });
 
-  test('Failed Login wrongPwd', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
-    await loginPage.login('felanvändare', 'felLösenord', "Consumer");
 
-    //await loginPage.assertFailedLogin();
-    //await expect(page.getByTestId('error-message')).toContainText('Incorrect password');
-    await expect(page.getByTestId('error-message')).toContainText("Incorrect password");
-  });
+//   test('Failed Login wrongPwd', async ({ page }) => {
+//     const loginPage = new LoginPage(page);
+//     await loginPage.goto();
+//     await loginPage.login('felanvändare', 'felLösenord', "Consumer");
 
-  test('Failed Login fieldMissing', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
-    await loginPage.login('', 'felLösenord');
+//     //await loginPage.assertFailedLogin();
+//     //await expect(page.getByTestId('error-message')).toContainText('Incorrect password');
+//     await expect(page.getByTestId('error-message')).toContainText("Incorrect password");
+//   });
 
-    //await loginPage.assertFailedLogin2();
-    await expect(page.getByTestId("error-message")).toHaveText("Please fill in all fields.");
-  });
+//   test('Failed Login fieldMissing', async ({ page }) => {
+//     const loginPage = new LoginPage(page);
+//     await loginPage.goto();
+//     await loginPage.login('', 'felLösenord');
 
-  test.only('Succesful Login', async ({ page }) => {
+//     //await loginPage.assertFailedLogin2();
+//     await expect(page.getByTestId("error-message")).toHaveText("Please fill in all fields.");
+//   });
+
+  test('Succesful Login', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     if (process.env.STORE_USER_PWD !== undefined){
@@ -54,19 +55,19 @@ import { LoginPage } from "../pages/loginPage";
     //await expect(page).toHaveURL("/store2/");
   });
 
-  test("logout", async({page}) => {
-    //first do login:
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
-    if (process.env.STORE_USER_PWD !== undefined){
-        password = process.env.STORE_USER_PWD;
-    }
-    await loginPage.login('joe', password, "Consumer");
-    await expect(page).toHaveURL("/store2/");
+//   test("logout", async({page}) => {
+//     //first do login:
+//     const loginPage = new LoginPage(page);
+//     await loginPage.goto();
+//     if (process.env.STORE_USER_PWD !== undefined){
+//         password = process.env.STORE_USER_PWD;
+//     }
+//     await loginPage.login('joe', password, "Consumer");
+//     await expect(page).toHaveURL("/store2/");
 
-    //then logout:
-    await loginPage.logout();
-    await expect(page).toHaveURL("/login/");
-  })
+//     //then logout:
+//     await loginPage.logout();
+//     await expect(page).toHaveURL("/login/");
+//   })
 
 //});
